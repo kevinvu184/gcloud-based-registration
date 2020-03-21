7ab2140706901bdd9d94e1d4032b212190812be8
Kevin Vu <kevinvu184@gmail.com>
2020-03-21 11:09:03 +1100

    Remove symfony

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,5 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'JsonException' => $vendorDir . '/symfony/polyfill-php73/Resources/stubs/JsonException.php',
 );
